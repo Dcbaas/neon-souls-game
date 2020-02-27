22b97410175dd3c6916a03d8df4991ce71132e33
Sierra Ellison <ellisosi@mail.gvsu.edu>
2020-02-27 01:15:53 -0500

    add overlay doc

--- neon-souls/overlay.py
+++ neon-souls/overlay.py
@@ -2,7 +2,17 @@ import league
 import pygame
 
 class Overlay(league.DUGameObject):
+    """
+    The overlay class handles the initialization and updating of the HP information overlay. 
+    HP information is taken from the current_health member of the player's health object. 
+    """
     def __init__(self, player):
+        """
+        Inits the overlay.
+
+        param - player: The player object whose HP will
+        be displayed in the overlay.
+        """
         super().__init__(self)
         self._layer = 1000
         self.player = player
@@ -19,6 +29,11 @@ class Overlay(league.DUGameObject):
         self.static = True
 
     def update(self, deltaTime):
+        """
+        Updates the overlay with the current health.
+        Updates the x coordinate of the overlay text to follow the player
+        across the map.
+        """
         self.image.fill((127, 127, 127))
         self.text = self.font.render(str(self.player.health.current_health) + " HP", True, (0,0,0))
         self.image.blit(self.text, (self.player.rect.centerx, 0))
